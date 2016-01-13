19f277a981ebd725d9c706da092ffde0a5aac512
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-01-13 00:22:47 -0300

    retrieve values commented.

--- example/js/diretor.js
+++ example/js/diretor.js
@@ -179,54 +179,54 @@ function busca(){
     
 
 
-    // Resgatar valores.
-    busca.prototype.resgatarValores = function(){
-        $('#resultado').html('Carregando dados...');
+//     // Resgatar valores.
+//     busca.prototype.resgatarValores = function(){
+//         $('#resultado').html('Carregando dados...');
 
-        // Estrutura de resultado.
-        $.getJSON('datas/informationDirectors.json', function(data){
-            this.qtd = data.directors.length;
-            this.nome = '';
-            this.urlFoto = '';
-            this.birthDate = '';
-            this.deathDate = '';
-            this.biografia = '';
+//         // Estrutura de resultado.
+//         $.getJSON('datas/informationDirectors.json', function(data){
+//             this.qtd = data.directors.length;
+//             this.nome = '';
+//             this.urlFoto = '';
+//             this.birthDate = '';
+//             this.deathDate = '';
+//             this.biografia = '';
 
-            buscaInputTermos = getParameterByName('director');
+//             buscaInputTermos = getParameterByName('director');
 
-            for (i = 0; i < this.qtd; i++){
-                if (buscaInputTermos == data.directors[i].FIELD1){
+//             for (i = 0; i < this.qtd; i++){
+//                 if (buscaInputTermos == data.directors[i].FIELD1){
 
-                    this.nome =  data.directors[i].FIELD1 + '<br />';
-                    this.urlFoto = '<img src=' + data.directors[i].FIELD2.trim() + '" class="img-responsive"><br />';
-                    this.birthDate =  data.directors[i].FIELD4 + '<br />';
-
-
-                    if (data.directors[i].FIELD5.trim() == "NA"){
-                        this.deathDate = '---';
-                    }else{
-                        this.deathDate =  data.directors[i].FIELD5.trim() + '<br />';
-                    }
-
-                    this.biografia = data.directors[i].FIELD3 + '<br /><br />';
-
-                }
+//                     this.nome =  data.directors[i].FIELD1 + '<br />';
+//                     //this.urlFoto = '<img src=' + data.directors[i].FIELD2.trim() + '" class="img-responsive"><br />';
+//                     this.birthDate =  data.directors[i].FIELD4 + '<br />';
 
 
-            }
+//                     if (data.directors[i].FIELD5.trim() == "NA"){
+//                         this.deathDate = '---';
+//                     }else{
+//                         this.deathDate =  data.directors[i].FIELD5.trim() + '<br />';
+//                     }
 
-            $('#nome').html(this.nome);
-            $('#urlFoto').html(this.urlFoto);
-            $('#birthDate').html(this.birthDate);
-            $('#deathDate').html(this.deathDate);
-            $('#biografia').html(this.biografia);
+//                     this.biografia = data.directors[i].FIELD3 + '<br /><br />';
 
-        });
+//                 }
 
-    }
 
-}
+//             }
 
-// Objeto.
-var obj = new busca();
-obj.resgatarValores();
+//             $('#nome').html(this.nome);
+//             //$('#urlFoto').html(this.urlFoto);
+//             $('#birthDate').html(this.birthDate);
+//             $('#deathDate').html(this.deathDate);
+//             $('#biografia').html(this.biografia);
+
+//         });
+
+//     }
+
+// }
+
+// // Objeto.
+// var obj = new busca();
+// obj.resgatarValores();
